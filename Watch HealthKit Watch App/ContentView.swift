d6fac79e124d97b9c40c59cde840203f7d267f36
WILLIAM SOUEF <williamsouef@gmail.com>
2023-02-05 15:08:04 +0100

    Initial Commit

---
+++ Watch HealthKit Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Watch HealthKit Watch App
+//
+//  Created by William Souef on 05/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watch HealthKit Watch App/Watch_HealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_HealthKitApp.swift
+//  Watch HealthKit Watch App
+//
+//  Created by William Souef on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Watch_HealthKit_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
